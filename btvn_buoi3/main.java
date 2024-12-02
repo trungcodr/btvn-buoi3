a48ab503245f8c6ce3346a3513ecece5ad0a9813
trung <92379274+trungcodr@users.noreply.github.com>
2024-12-02 10:06:17 +0700

    nop btvn buoi 3

---
+++ btvn_buoi3/main.java
@@ -0,0 +1,56 @@
+package btvn_buoi3;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhap a: ");
+//        double a = Double.parseDouble(scanner.nextLine());
+//        System.out.println("Nhap b: ");
+//        double b = Double.parseDouble(scanner.nextLine());
+//        System.out.println("Phep toan: " + "+:add " + " -:sub " + " *:mul " + " /:div " + " %:rem ");
+//        System.out.println("Phep toan ban muon thuc hien: ");
+//        String type = scanner.nextLine();
+//        switch (type){
+//            case "add":
+//                System.out.println("Phep cong hai so a va b la: " + (a+b));
+//                break;
+//            case "sub":
+//                System.out.println("Phep tru hai so a va b la: " + (a-b));
+//                break;
+//            case "mul":
+//                System.out.println("Phep nhan hai so a va b la: " + (a*b));
+//                break;
+//            case "div":
+//                System.out.println("Phep chia hai so a va b la: " + (a/b));
+//                break;
+//            case "rem":
+//                System.out.println("Phep lay du hai so a va b la: " + (a%b));
+//                break;
+//            default:
+//                System.out.println("Khong hop le!");
+//        }
+//  ------------------------------------------------------Bai 9---------------------------------------------------------
+//        String name = "";
+//        String adress = "";
+//        int age = 0;
+//        String choose = "";
+//        do {
+//            System.out.println("Moi ban nhap ten cua minh: ");
+//            name = scanner.nextLine();
+//            System.out.println("Moi ban nhap dia chi cua minh: ");
+//            adress = scanner.nextLine();
+//            System.out.println("Moi ban nhap tuoi cua minh: ");
+//            age = Integer.parseInt(scanner.nextLine());
+//            System.out.println("Ten cua ban la: " + name);
+//            System.out.println("Dia chi cua ban la: " + adress);
+//            System.out.println("Tuoi cua ban la: " + age);
+//
+//            System.out.println("Do you want to continue?(Y/N) ");
+//            choose = scanner.nextLine();
+//
+//        }
+//        while (choose.equalsIgnoreCase("y"));
+//        System.out.println("Stop the program");
+
+    }
+}
